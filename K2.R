4f36a3dee19a505d6a8a075b09b37914787803c9
odk022 <odk022@uit.no>
2024-08-26 07:15:28 +0200

    ny

--- K2.R
+++ K2.R
@@ -22,3 +22,39 @@ spss_data<- read_spss("spss_data.sav")
 stata_data<- read_stata("stata_data.dta")
 
 # Kan bruke direkte-funksjonene på menyene til å gjøre det samme "Import Dataset"
+
+# Finner ikke fila "loenn"
+
+#loenn_data<- read_stata("loenn.dta")
+
+#save(loenn_kvinne, file="mydata.Rdata")
+
+# Legge inn data:
+# Lager en tom tabell:
+loenn_data<- data.frame(respid=numeric(0),
+                        tloenn=numeric(0),
+                        alder=numeric(0),
+                        kjonn=character(0),
+                        utdann=numeric(0))
+fix(loenn_data)
+
+# Legger inn data:
+respid<- c(1,2,3,4,6)
+tloenn<-c(270,330,655,455,150)
+alder<-c(34,46,51,39,22)
+kjonn<-c("mann","kvinne","mann","mann","kvinne")
+utdann<-c(10,12,15,13,8)
+loenn_data<- data.frame(respid,tloenn,alder,kjonn,utdann)
+View(loenn_data)
+
+# Enklere måte:
+loenn_data<- data.frame(
+  respid=c(1,2,3,4,6),
+  tloenn=c(270,330,655,455,150),
+  alder=c(34,46,51,39,22),
+  kjonn=c("mann","kvinne","mann","mann","kvinne"),
+  utdann=c(10,12,15,13,8)
+)
+loenn_data
+save(loenn_data, file = "loenn_data.Rdata")
+load(file = "loenn_data.Rdata")
