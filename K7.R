c6df2beb32c12d229088c62e238718ef2647e561
odk022 <odk022@uit.no>
2024-10-28 20:04:45 +0100

    Ferdig

--- K7.R
+++ K7.R
@@ -80,3 +80,75 @@ confint(model03)
 # snitt er 763 kroner unna estimert/predikert gave_verdi
 # - Høy justert R^2 tilsier at modellen passer godt til dataene
 # - F_testen. Vi ser at p-verdien er langt under sigifikansnivået på 5% 
+
+# Verdien på de standariserte tilsier at de har stor effekt.
+# sammenlikne størrelsen på disse ved å bruke glht():
+
+library(multcomp)
+comp1<- glht(model03,linfct = c("attraktiv - snill = 0"))
+summary(comp1)
+
+comp2<- glht(model03,linfct = c("attraktiv - alder = 0"))
+summary(comp2)
+
+comp3<- glht(model03,linfct = c("snill - alder = 0"))
+summary(comp3)
+
+# Kan bestemme den relative viktigheten til uavhengige variabler ved å bestemme den 
+# kvadrerte semipartielle korrelasjon.
+library(relaimpo)
+calc.relimp(model02, type="last")
+# Vi ser at de uavhengige variablene forklarer henholdsvis 11, 9 og 8 % av 
+# variasjonen i den avhengige variablen 
+
+# Prediksjoner
+xsvals<- data.frame(attraktiv=7, snill=7, alder=50)# setter verdier på variablene
+predval <- predict(model02, newdata = xsvals, 
+                   interval = "confidence", level = 0.95)
+xsvals_pval <- cbind(xsvals, predval)
+xsvals_pval
+
+# Se på hva skjer med en variabel når de andre holdes konstant:
+xsvals2 <- data.frame(attraktiv=c(1,2,3,4,5,6,7), 
+                      snill=mean(gave$snill), 
+                      alder=mean(gave$alder))
+predval2 <- predict(model02, newdata = xsvals2, 
+                    interval = "confidence", 
+                    level = 0.95)
+xsvals_pval2 <- cbind(xsvals2, predval2)
+xsvals_pval2
+
+# Dette kan vi plotte:
+ggplot(xsvals_pval2, aes(x=attraktiv, y=fit)) +
+  geom_smooth(aes(ymin = lwr, ymax = upr), 
+              stat = "identity") 
+
+# Kan lage fine tabeller med stargazer:
+library(stargazer)
+stargazer(model02, type="text", 
+          keep.stat=c("n", "rsq"),
+          out="model.txt")
+
+# Regresjonsdianostikk
+# handler om hvorvidt en modell passer til dataene
+# Forutsetningene:
+# 1. additivitet og linearitet
+# 2. uavhengighet mellom feilledd (hvis dataene dwlvis kommer fra samme person er dette brutt)
+# 3. lik varians av feilleddet(homoskedastisitet) (lik for alle verdier av den uavhengige var)
+# 4. normalfordeling av feilleddene
+# viktist er linearitet
+
+# sjekke utliggere og disse sin betydning for den avhengige variablen
+# bruker plot()
+plot(model02)
+# Oversikt
+regression.diagnostics(model02)
+
+gave2 <- gave %>%
+  add_row(gave_verdi=20000, attraktiv=1, snill=8, alder=2)
+model02.utligger <- lm(gave_verdi ~ attraktiv + snill + alder, 
+                       data=gave2)
+regression.diagnostics(model02.utligger) 
+
+
+
